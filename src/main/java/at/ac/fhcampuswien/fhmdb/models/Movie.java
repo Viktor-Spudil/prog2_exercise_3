633cb742f7dc628750487ad133a65b3c0843c778
Viktor Spudil <viktor.spudil@stud.fh-campuswien.ac.at>
2023-04-17 15:50:06 +0200

    MovieAPI Klasse, UI adaptieren, Java Streams

--- src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/models/Movie.java
@@ -5,21 +5,34 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Movie {
+    public final String id;
     public final String title;
-    public final String description;
     public final List<Genre> genres;
     public Integer releaseYear;
-    public List<String> mainCast;
+    public final String description;
+    public final String imgUrl;
+    public final Integer lengthInMinutes;
     public List<String> directors;
+    public List<String> writers;
+    public List<String> mainCast;
+    public Double rating;
 
 
-    public Movie(String title, String description, List<Genre> genres) {
+
+
+
+    public Movie(String id, String title, List<Genre> genres, Integer releaseYear, String description, String imgUrl, Integer lengthInMinutes, List<String> directors, List<String> writers, Double rating) {
+        this.id = id;
         this.title = title;
-        this.description = description;
         this.genres = genres;
-        this.releaseYear = null;
+        this.releaseYear = releaseYear;
+        this.description = description;
+        this.imgUrl = imgUrl;
+        this.lengthInMinutes = lengthInMinutes;
+        this.directors = directors;
+        this.writers = writers;
         this.mainCast = new ArrayList<>();
-        this.directors = new ArrayList<>();
+        this.rating = rating;
     }
 
 
@@ -37,26 +50,47 @@ public class Movie {
         return this.title.equals(other.title) && this.description.equals(other.description) && this.genres.equals(other.genres);
     }
 
-    public String getTitle() {
-        return title;
+    public String getId() {
+        return this.id;
     }
 
-    public String getDescription() {
-        return description;
+    public String getTitle() {
+        return this.title;
     }
 
     public List<Genre> getGenres() {
-        return genres;
+        return this.genres;
     }
 
     public Integer getReleaseYear() {
-        return releaseYear;
+        return this.releaseYear;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getImgUrl() {
+        return this.imgUrl;
+    }
+
+    public Integer getLengthInMinutes() {
+        return this.lengthInMinutes;
+    }
+
+    public List<String> getDirectors() {
+        return this.directors;
+    }
+
+    public List<String> getWriters() {
+        return this.writers;
     }
 
     public List<String> getMainCast() {
-        return mainCast;
+        return this.mainCast;
     }
-    public List<String> getDirectors() {
-        return directors;
+
+    public Double getRating() {
+        return this.rating;
     }
 }
