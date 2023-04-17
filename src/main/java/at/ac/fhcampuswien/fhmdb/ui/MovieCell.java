ea4b46287c340f2656e384c6b7404f4408774800
Viktor Spudil <viktor.spudil@stud.fh-campuswien.ac.at>
2023-04-17 16:16:28 +0200

    MovieAPI Klasse, UI adaptieren, Java Streams

--- src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/ui/MovieCell.java
@@ -14,7 +14,9 @@ public class MovieCell extends ListCell<Movie> {
     private final Label title = new Label();
     private final Label detail = new Label();
     private final Label genre = new Label();
-    private final VBox layout = new VBox(title, detail, genre);
+    private final Label releaseYear = new Label();
+    private final Label rating = new Label();
+    private final VBox layout = new VBox(title, detail, genre, releaseYear, rating);
 
     @Override
     protected void updateItem(Movie movie, boolean empty) {
@@ -38,12 +40,17 @@ public class MovieCell extends ListCell<Movie> {
                     .collect(Collectors.joining(", "));
             genre.setText(genres);
 
+            releaseYear.setText("Release Year: " + movie.getReleaseYear());
+            rating.setText("Rating: " + movie.getRating());
+
 
             // color scheme
             title.getStyleClass().add("text-yellow");
             detail.getStyleClass().add("text-white");
             genre.getStyleClass().add("text-white");
             genre.setStyle("-fx-font-style: italic");
+            releaseYear.getStyleClass().add("text-white");
+            rating.getStyleClass().add("text-white");
             layout.setBackground(new Background(new BackgroundFill(Color.web("#454545"), null, null)));
 
             // layout
