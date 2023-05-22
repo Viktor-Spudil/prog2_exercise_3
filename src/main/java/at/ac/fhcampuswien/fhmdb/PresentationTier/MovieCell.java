8c69fc83cf783587964e01afd81e4ea1647904c4
Viktor Spudil <viktor.spudil@stud.fh-campuswien.ac.at>
2023-05-22 12:32:08 +0200

    Task 1, 2, 3

--- src/main/java/at/ac/fhcampuswien/fhmdb/PresentationTier/MovieCell.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/PresentationTier/MovieCell.java
@@ -89,7 +89,7 @@ public class MovieCell extends ListCell<Movie> {
 
             // layout
             title.fontProperty().set(title.getFont().font(20));
-            //detail.setMaxWidth(this.getScene().getWidth() - 30);
+            //detail.setMaxWidth(this.getScene().getWidth() - 30);               //Hat hin und wieder Exceptions verursacht, wahrscheinlich wegen falsch synchronisierter nebenläufigkeit
             detail.setWrapText(true);
             layout.setPadding(new Insets(10));
             layout.spacingProperty().set(10);
